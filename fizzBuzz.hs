5253a67692269725efcdc4489973974486c0175d
quantum33 <thierry.castel33@gmail.com>
2020-11-02 22:09:49 +0100

    Update fizzBuzz.hs

--- fizzBuzz.hs
+++ fizzBuzz.hs
@@ -1,9 +1,9 @@
-isFizzBuzz x = isFizz x && isBuzz x
-isFizz x = x `mod` 3 == 0
-isBuzz x = x `mod` 5 == 0
-
 fizzBuzz l m = [
     if  isFizzBuzz x then "fizz-buzz"
     else if isFizz x then "fizz"
     else if isBuzz x then "buzz"
     else show x | x <-[l..m]]
+
+isFizzBuzz x = isFizz x && isBuzz x
+isFizz x = x `mod` 3 == 0
+isBuzz x = x `mod` 5 == 0
